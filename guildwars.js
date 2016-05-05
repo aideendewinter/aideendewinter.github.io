32bba300726d44639d708f737974b54b7d4e9f7b
jesswhitworth <jessewhitworth@gmail.com>
2016-05-05 18:00:19 -0400

    Update guildwars.js

--- guildwars.js
+++ guildwars.js
@@ -1,29 +1,29 @@
 // http://stackoverflow.com/questions/30008114/how-do-i-promisify-native-xhr
 function makeRequest (method, url) {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.onload = function () {
-      if (this.status >= 200 && this.status < 300) {
-        resolve({
-			respone: xhr.response,
-			pageCount: xhr.getResponseHeader("X-Page-Total")
-		});
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText
-      });
-    };
-    xhr.send();
-  });
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				resolve({
+					respone: xhr.response,
+					pageCount: xhr.getResponseHeader("X-Page-Total")
+				});
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function () {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+		xhr.send();
+	});
 }
 
 // My code.
